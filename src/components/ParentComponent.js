809015d6d8c66a3784fc2e59343fa103b2d81ef9
Akash <akashikur011@gmail.com>
2023-08-21 21:03:52 +0530

    first

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import ChildComponent from "./ChildComponent";
+
+function ParentComponent() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Write code", completed: false },
+    { id: 3, text: "Go for a walk", completed: false },
+  ]);
+
+  const handleCompleteTodo = (todoId) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === todoId ? { ...todo, completed: true } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <ChildComponent todos={todos} onCompleteTodo={handleCompleteTodo} />
+    </div>
+  );
+}
+
+export default ParentComponent;
